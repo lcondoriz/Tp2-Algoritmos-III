7f204669b6ce72eda797ef2568addffbf255971a
marcos <wengxumarcostomas@gmail.com>
2023-11-27 14:22:29 -0300

    Revert "Dependencias para LectorJSON"
    This reverts commit 1dea585f5e2717b89ee217e797c5821ef6772edd.

--- src/main/java/edu/fiuba/algo3/lector/LectorJSON.java
+++ src/main/java/edu/fiuba/algo3/lector/LectorJSON.java
@@ -1,27 +1,8 @@
 package edu.fiuba.algo3.lector;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.File;
-import java.io.IOException;
-
 public class LectorJSON implements Lector {
     @Override
-    public void leerArchivo(String nombreArchivo) {
-        try {
-            
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(new File(nombreArchivo));
-
-            procesarJsonNode(jsonNode);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void procesarJsonNode(JsonNode jsonNode) {
-        
+    public void leerArchivo(String nombreArchivo){
+        //Implementación para leer archivo JSON
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,4 @@
 module edu.fiuba.algo3 {
     requires javafx.controls;
-    requires com.fasterxml.jackson.databind;
     exports edu.fiuba.algo3;
 }
