0c229837c0ba27919be86d80642860b202a0a8c2
lcondoriz <lcondoriz@fi.uba.ar>
2023-12-21 15:59:12 -0300

    test: Juego, TableroConstructor.

--- src/main/test/edu/fiuba/algo3/modeloTest/JuegoTest.java
+++ src/main/test/edu/fiuba/algo3/modeloTest/JuegoTest.java
@@ -2,13 +2,16 @@ package edu.fiuba.algo3.modeloTest;
 
 
 import edu.fiuba.algo3.modelo.exceptions.CantidadJugadoresException;
+import edu.fiuba.algo3.modelo.exceptions.CantidadTurnosException;
 import edu.fiuba.algo3.modelo.exceptions.NoHayJugadoresException;
+import edu.fiuba.algo3.modelo.exceptions.PartidaFinalizada;
 import edu.fiuba.algo3.modelo.juego.AlgoRoma;
 import edu.fiuba.algo3.modelo.juego.Dado;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class JuegoTest {
@@ -65,5 +68,36 @@ public class JuegoTest {
 
         // Act & Assert
         assertDoesNotThrow(algoRoma::jugar1Ronda);
+    }
+
+    @Test
+    public void testNoHayGanador() {
+        // Arrange
+        algoRoma.cargarTablero("files/mapa.json");
+
+        algoRoma.agregarJugador("Jugador 1");
+        algoRoma.agregarJugador("Jugador 2");
+        algoRoma.inicializarJuego();
+
+        when(dado.lanzar()).thenReturn(0); // El jugador no se mueve, asi llega a perder todos sus turnos
+
+        // Act & Assert
+        CantidadTurnosException exception = assertThrows(CantidadTurnosException.class, algoRoma::jugar);
+        assertEquals("No hay ganador ya que superaron la maxima cantidad de rondas.", exception.getMessage());
+    }
+
+    @Test
+    public void testHayGanador() {
+        // Arrange
+        algoRoma.cargarTablero("files/mapa.json");
+
+        algoRoma.agregarJugador("Jugador 1");
+        algoRoma.agregarJugador("Jugador 2");
+        algoRoma.inicializarJuego();
+
+        when(dado.lanzar()).thenReturn(2);
+
+        // Act & Assert
+        assertThrows(PartidaFinalizada.class, algoRoma::jugar);
     }
 }
--- src/main/test/edu/fiuba/algo3/modeloTest/parser/TableroConstructorTest.java
+++ src/main/test/edu/fiuba/algo3/modeloTest/parser/TableroConstructorTest.java
@@ -1,15 +1,19 @@
 package edu.fiuba.algo3.modeloTest.parser;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import edu.fiuba.algo3.modelo.parser.TableroConstructor;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TableroConstructorTest {
     @Test
-    public void test01validarConstruirTableroDesdeJSON() {
+    public void testValidarConstruirTableroDesdeJSON() {
         // Arrange
         TableroConstructor tableroConstructor = new TableroConstructor();
 
@@ -17,7 +21,7 @@ public class TableroConstructorTest {
         assertDoesNotThrow(() -> tableroConstructor.construirTableroDesdeJSON("files/mapa.json"));
     }
     @Test
-    public void test02validarAnchoYlargoNumerosPositivos() {
+    public void testValidarAnchoYlargoNumerosPositivos() {
         // Arrange
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("ancho", 10);
@@ -32,7 +36,22 @@ public class TableroConstructorTest {
         assertEquals(true, resultado);
     }
     @Test
-    public void test03validarFormatoCelda() {
+    public void testValidarAnchoYlargoNumerosNegativos() {
+        // Arrange
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("ancho", -10);
+        jsonObject.addProperty("largo", -10);
+
+        TableroConstructor tableroConstructor = new TableroConstructor();
+
+        // Act
+        boolean resultado = tableroConstructor.validarAnchoYlargo(jsonObject);
+
+        // Assert
+        assertEquals(false, resultado);
+    }
+    @Test
+    public void testValidarFormatoCelda() {
         // Arrange
         JsonObject jsonCelda = new JsonObject();
         jsonCelda.addProperty("x", 1);
@@ -48,10 +67,27 @@ public class TableroConstructorTest {
 
         // Assert
         assertEquals(true, resultado);
-
     }
     @Test
-    public void test03validarTipoCelda() {
+    public void testValidarFormatoCeldaConObstaculoInvalido() {
+        // Arrange
+        JsonObject jsonCelda = new JsonObject();
+        jsonCelda.addProperty("x", 1);
+        jsonCelda.addProperty("y", 1);
+        jsonCelda.addProperty("tipo", "Salida");
+        jsonCelda.addProperty("obstaculo", "ObstaculoInvalido");
+        jsonCelda.addProperty("premio", "");
+
+        TableroConstructor tableroConstructor = new TableroConstructor();
+
+        // Act
+        boolean resultado = tableroConstructor.validarFormatoCelda(jsonCelda);
+
+        // Assert
+        assertEquals(false, resultado);
+    }
+    @Test
+    public void testValidarTipoCelda() {
         // Arrange
         JsonObject jsonCelda = new JsonObject();
         jsonCelda.addProperty("x", 1);
@@ -68,5 +104,34 @@ public class TableroConstructorTest {
         // Assert
         assertEquals(true, resultado);
     }
+    @Test
+    public void testValidarFormatoMapa() throws IOException {
+        // Arrange
+        String path = "files/mapaTest.json";
+        FileReader fileReader = new FileReader(path);
+        Gson gson = new Gson();
+        JsonObject jsonTablero = gson.fromJson(fileReader, JsonObject.class);
+
+        // Act
+        // Cambiar el ancho a -10 para que falle la validación.
+        jsonTablero.getAsJsonObject("mapa").addProperty("ancho", -10);
+
+        // Guardar el archivo modificado
+        try (FileWriter fileWriter = new FileWriter(path)) {
+            gson.toJson(jsonTablero, fileWriter);
+        }
+
+        TableroConstructor tableroConstructor = new TableroConstructor();
+
+        // Assert
+        assertThrows(IllegalArgumentException.class, () -> tableroConstructor.construirTableroDesdeJSON("files/mapaTest.json"));
+
+        // Volver a dejar el archivo como estaba
+        jsonTablero.getAsJsonObject("mapa").addProperty("ancho", 10);
+        try (FileWriter fileWriter = new FileWriter(path)) {
+            gson.toJson(jsonTablero, fileWriter);
+        }
+    }
 }
+
 
